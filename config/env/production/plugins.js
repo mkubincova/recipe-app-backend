fda6c6d2f97e1c88a80fc3ee3327d9649b019156
mkubincova <mkubincova@proton.me>
2023-06-29 13:08:56 +0200

    feat: cloudinary

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = ({ env }) => ({
   graphql: {
     config: {
       endpoint: '/graphql',
@@ -12,4 +12,19 @@ module.exports = {
       },
     },
   },
-};
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
